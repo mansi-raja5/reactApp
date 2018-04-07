77d3bcfd9cd7e5e03800a91a9c02be2a1e759ddc
Mansi Raja <mansi.raja5@gmail.com>
2018-04-08 01:06:19 +0530

    Add Inline CSS to react

--- App.jsx
+++ App.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 class App extends React.Component{
 	render(){
 		var i = 1;
+		
+		var myStyle = {
+			fontSize: 100,
+			color : "red"
+		};
+		
 		return(
 			<div>
-				<h1>Header</h1>
+				<h1 style = {myStyle}>Header</h1>
 				<h2>Content</h2>
 				<p data-myattribute = "somevalue">This is the content!!!</p>
 				Hello World!
