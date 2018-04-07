e90da5f06bd6b64f4010f0663aa8ee0c80d8995e
Mansi Raja <mansi.raja5@gmail.com>
2018-04-08 01:03:44 +0530

    Some react syntax

--- App.jsx
+++ App.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 class App extends React.Component{
 	render(){
+		var i = 1;
 		return(
 			<div>
+				<h1>Header</h1>
+				<h2>Content</h2>
+				<p data-myattribute = "somevalue">This is the content!!!</p>
 				Hello World!
+				<br/>
 				Mansi Raja
+				<br/>
+				2+2 = {2+2}
+				<br/>
+				2*4 = {2*4}
+				<br/>
+				<br/>
+				{i==1 ? "i is having 1 value" : "no value"}
 			</div>
 		);
 	}
