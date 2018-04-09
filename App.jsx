8df7b8460c57eb578ea4fd85898a46e279228137
Mansi Raja <mansi.raja5@gmail.com>
2018-04-09 16:39:02 +0530

    set state example

--- App.jsx
+++ App.jsx
@@ -25,9 +25,24 @@ class App extends React.Component{
 					"name":"baz",
 					"age":30
 				},				
-			]
+			],
+            myData : [],
+            mansi : 0
 		}
-	}
+		this.setStateHandler = this.setStateHandler.bind(this);
+		this.mansiClickEvent = this.mansiClickEvent.bind(this);
+	};
+    setStateHandler(){
+        var item = "setState..."
+        var myArray = this.state.myData.slice();
+        myArray.push(item);
+        this.setState({myData: myArray})
+    };
+    mansiClickEvent(){
+        var mInc = this.state.mansi;
+        mInc++;
+        this.setState({mansi:mInc})
+    };
 	render(){
 		var i = 1;
 		
@@ -78,6 +93,18 @@ class App extends React.Component{
                 <h3>Func: {this.props.propFunc(3)}</h3>
                 <h3>Number: {this.props.propNumber}</h3>
                 <h3>String: {this.props.propString}</h3>
+
+                =================================================
+                <br/>
+                <button onClick = {this.setStateHandler}>SET STATE</button>
+                <h4>State Array: {this.state.myData}</h4>
+                =================================================
+                <br/>
+                <button onClick = {this.mansiClickEvent}>Mansi Click</button>
+                <h4>Mansi Value: {this.state.mansi}</h4>
+                =================================================
+                <br/>
+
 			</div>
 		);
 	}
