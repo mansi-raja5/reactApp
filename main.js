a007a194f1d7144d6ce588ad41a689506b3576e8
Mansi Raja <mansi.raja5@gmail.com>
2018-04-09 19:48:43 +0530

    Get text box value onchange

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import MyComp from "./myComp.jsx";
+import Form from "./form.jsx";
 
 ReactDOM.render(<App headerProp = "Header from props...." contentProp= "Contents from props..."/>, document.getElementById('app'));
 
 ReactDOM.render(<MyComp/>, document.getElementById('mycomp'));
+
+ReactDOM.render(<Form/>,document.getElementById('myform'));
